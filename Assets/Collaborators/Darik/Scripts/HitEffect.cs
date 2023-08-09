40c3715a035cc5dead52e791b520c1e2ffb2b20f
소윤진 <sohyoonjin1127@gmail.com>
2023-08-09 19:59:54 +0900

    feat : 적 피격 이펙트 오브젝트 풀링 적용

---
+++ Assets/Collaborators/Darik/Scripts/HitEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffect : MonoBehaviour
+{
+    private float curTime;
+
+    private void Update()
+    {
+        curTime += Time.deltaTime;
+        if (curTime > 1f)
+            GameManager.Resource.Destroy(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        curTime = 0f;
+    }
+}
