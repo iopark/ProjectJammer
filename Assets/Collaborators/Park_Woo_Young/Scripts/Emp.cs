056ea144789dcf2b6af396c296696a8de93c5791
Geundol222 <geundol222@gmail.com>
2023-08-14 20:10:12 +0900

    feat : 발전기 조건수정

---
+++ Assets/Collaborators/Park_Woo_Young/Scripts/Emp.cs
@@ -0,0 +1,28 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Park_Woo_Young
+{
+    public class Emp : MonoBehaviourPunCallbacks, Darik.IHittable
+    {
+
+        [SerializeField] LayerMask targetEnemy;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == targetEnemy)
+            {
+
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void TakeDamage(int damage, Vector3 hitPoint, Vector3 normal)
+        {
+        }
+    }
+}
+
