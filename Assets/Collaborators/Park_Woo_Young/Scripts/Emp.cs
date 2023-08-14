2be445221d0c6d26776ad3bbd591dbe8315fe375
Geundol222 <geundol222@gmail.com>
2023-08-14 21:51:19 +0900

    feat : 함수위치

--- Assets/Collaborators/Park_Woo_Young/Scripts/Emp.cs
+++ Assets/Collaborators/Park_Woo_Young/Scripts/Emp.cs
@@ -1,29 +1,29 @@
 using Darik;
+using ildoo;
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace Park_Woo_Young
 {
-    public class Emp : MonoBehaviourPunCallbacks, Darik.IHittable
+    public class Emp : MonoBehaviourPunCallbacks //, Darik.IHittable
     {
 
         [SerializeField] LayerMask targetEnemy;
 
-        private void OnTriggerEnter(Collider other)
+        public void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.layer == targetEnemy)
+            if (targetEnemy.Contain(other.gameObject.layer))
             {
-               //gameObject
-            }
-            else
-            {
-                return;
-            }
+                //other.gameObject.GetComponent<IHittable>();
+                Debug.Log("sdsds");
+            }      
         }
-        public void TakeDamage(int damage, Vector3 hitPoint, Vector3 normal)
+
+        /*public void TakeDamage(int damage, Vector3 hitPoint, Vector3 normal)
         {
-        }
+            
+        }*/
     }
 }
 
