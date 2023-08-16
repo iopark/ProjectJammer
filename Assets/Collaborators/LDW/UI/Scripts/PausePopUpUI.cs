531bdfc7dbe91f1e7210a047f1243bde80f2eb04
jungang11 <94428775+jungang11@users.noreply.github.com>
2023-08-16 19:14:01 +0900

    feat : GameOver, Pause PopUpUI Script

--- Assets/Collaborators/LDW/UI/Scripts/PausePopUpUI.cs
+++ Assets/Collaborators/LDW/UI/Scripts/PausePopUpUI.cs
@@ -1,22 +1,22 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace LDW
 {
-    public class GameOver : PopUpUI
+    public class PausePopUpUI : PopUpUI
     {
         protected override void Awake()
         {
             base.Awake();
 
-            buttons["LobbyButton"].onClick.AddListener(() => { LobbyButton(); });
+            buttons["ResumeButton"].onClick.AddListener(() => { GameManager.UI.ClosePopUpUI(); });
+            buttons["LeaveButton"].onClick.AddListener(() => { LeaveButton(); });
         }
 
-        public void LobbyButton()
+        public void LeaveButton()
         {
             PhotonNetwork.AutomaticallySyncScene = false;
             // PhotonNetwork.LocalPlayer.SetLoad(false);
@@ -25,6 +25,17 @@ namespace LDW
 
             SceneManager.LoadScene("LobbySceneLDW-0816-1620");
             GameManager.UI.ClosePopUpUI();  // ÆË¾÷ ´Ý±â(GameOverPanel)
+        }
+
+        public void Test()
+        {
+            if (GameManager.Data.DisruptorProgress == 100)
+                return;
+
+            if (GameManager.UI.popUp_Stack.Count > 0)
+                GameManager.UI.ClosePopUpUI();
+
+            GameManager.UI.ShowPopUpUI<PopUpUI>("PausePopUpUI");
         }
     }
 }
